7af14cda8caf79e640f32313a6a26d9485600b35
Suraj Adhikari <heartbreakkidhbk97@gmail.com>
2019-08-03 11:32:15 +0545

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Hello From E-Commerce !!</h1>
+    </Fragment>
+  );
+}
+
+export default App;
