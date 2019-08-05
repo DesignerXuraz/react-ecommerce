53e11d7cea13014bf9829788efc10660dfb9a58e
Suraj Adhikari <heartbreakkidhbk97@gmail.com>
2019-08-05 08:08:12 +0545

    about page completed

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,45 @@
 import React, { Fragment } from "react";
-
+import aboutBcg from "../images/aboutBcg.jpeg";
+import defaultBcg from "../images/defaultBcg.jpeg";
+import Hero from "../components/Hero";
+import Title from "../components/Title";
 const AboutPage = () => {
   return (
     <Fragment>
-      <p>Hello From AboutPage</p>
+      <Hero img={aboutBcg} />
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-10 my-6 mx-auto col-md-6">
+            <img
+              src={defaultBcg}
+              className="img-fluid img-thumbnail"
+              alt="about"
+            />
+          </div>
+          <div className="col-10 my-6 mx-auto col-md-6 my-3">
+            <Title title="about us" />
+            <p className="text-lead text-muted my-22">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              quisquam illo voluptas in tempora ex, exercitationem excepturi
+              repudiandae dignissimos nam.
+            </p>
+            <p className="text-lead text-muted my-22">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              quisquam illo voluptas in tempora ex, exercitationem excepturi
+              repudiandae dignissimos nam.
+            </p>
+            <button
+              type="button"
+              className="main-link"
+              style={{ marginTop: "1rem" }}
+            >
+              <span style={{ color: "black", textTransform: "capitalize" }}>
+                More Info
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
